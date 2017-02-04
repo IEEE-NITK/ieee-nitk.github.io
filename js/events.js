10d625ab8645e13610928c98c5196cf4fbe7bc3d
Vighnesh <vighneshvelayudhan@gmail.com>
2017-02-04 17:28:50 +0530

    events page made responsive

--- js/events.js
+++ js/events.js
@@ -1,20 +1,28 @@
-$(document).ready(function(){
-	console.log("this was called for");
-	$("#event_list li").mouseenter(function(event){
-		$(".event_details h3").html(event.target.id);
-	}).click(function(event){
-		console.log(event.target.id);
-		$(".box_left").animate({
-				"padding":"0px",
-				"width":"10%"
-			},
-			{
-				duration : 400, 
-				queue : false
-			});
-		$(".box_middle").animate({"width":"40%","padding":"0px"},{duration:400,queue:false});
-		$(".box_left .text2").css({"transform":"rotate(-90deg)"},{duration:400,queue:false});
-		$(".box_left .event_details").css({"display":"none"});
+$(document).ready(function() {
 
-	});
+    var mobile = $(document).outerWidth(true) < 768 ? true : false;
+
+    $("#event_list li").mouseenter(function(event) {
+        $(".event_details h3").html(event.target.id);
+    }).click(function(event) {
+        if (!mobile) {
+            if ($(".box_left .event_details").css("display") != "none") {
+                $(".box_left").animate({
+                    "padding": "0px",
+                    "width": "10%"
+                }, {
+                    duration: 400,
+                    queue: false
+                });
+                $(".box_middle").animate({ "width": "40%", "padding": "0px" }, { duration: 400, queue: false });
+                $(".box_left .text2").css({ "transform": "rotate(-90deg)" }, { duration: 400, queue: false });
+                $(".box_left .event_details").css({ "display": "none" });
+            }
+        } else {
+            //the mobile code
+            $(".box_middle").addClass("visible_side_menu");
+            console.log("that wont work here bhai");
+        }
+
+    });
 });
