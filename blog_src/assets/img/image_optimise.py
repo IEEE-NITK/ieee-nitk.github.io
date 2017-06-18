5a4a6772868124f786abdcbed68324b430ffcae0
Vilas M <vilasnitk19@gmail.com>
2017-06-18 18:18:30 +0530

    New Post: Introduction to reverse engineering (#18)
    * rev engg article added
    
    * mohita article added

--- blog_src/assets/img/image_optimise.py
+++ blog_src/assets/img/image_optimise.py
@@ -17,9 +17,9 @@ with Image.open(file_name) as img:
 	width,height = img.size
 	print(str(width) + " " + str(height))
 
-	if height > 600:
-		new_width = ((int)((width/height)*600))
-		img = img.resize((new_width,600), Image.ANTIALIAS)
+	if height > 200:
+		new_width = ((int)((width/height)*200))
+		img = img.resize((new_width,200), Image.ANTIALIAS)
 
 	print("Image size after resizing")
 	width,height = img.size
