0ad0ce02a6f979bbd6314636d9656403656da816
gaurang2001 <gaurangvelingkar@gmail.com>
2020-12-20 21:01:45 +0530

    TODO: Navigation bar; basic CSS done, Dropdown not showing

--- js/header.js
+++ js/header.js
@@ -4,32 +4,28 @@ function displayHeader() {
     $.ajax({
         url: jsonurl,
         success: function (result) {
-            var htmlStr = '<a class="logo" href="https://www.ieee.org/" title="home" onclick="$.fn.fullpage.moveTo(1)">\
-            <img id="small_logo" src="res/img/ieee_nitk.png" alt="IEEE Logo"></a>\
-            <div class="popup_menu"><ul id="menu" class="teco">';
+            var htmlStr = '<div class="col-md-2">\
+            <div id="colorlib-logo"><a href="https://www.ieee.org/" title="home" onclick="$.fn.fullpage.moveTo(1)">\
+            <img src="res/img/ieee_nitk.png" alt="IEEE Logo" height="50"></a></div>\
+            </div>\
+            <div class="col-md-10 text-right menu-1"><ul>';;
             for (var i = 0; i < result.length; i++) {
                 if (result[i].name!="SIGs"){
                 htmlStr += '<li>\
                 <a href="' + result[i].link + '">' + result[i].name + '</a></li>';
                 }
                 else{
-                    htmlStr += '<li>\
-                    <div class="dropdown">\
-                        <button class="dropbtn">SIGs</button>\
-                        <div class="dropdown-content">';
+                    htmlStr += '<li class="has-dropdown">\
+                        <a>SIGs</a>\
+                        <ul class="dropdown">';
                     for(var j=0; j<result[i].sigs.length;j++){
-                        htmlStr += '<a href="' + result[i].sigs[j].link + '">' + result[i].sigs[j].name + '</a>';
+                        htmlStr += '<li><a href="' + result[i].sigs[j].link + '">' + result[i].sigs[j].name + '</a></li>';
                     }
-                    htmlStr += '</div></div></li>';
+                    htmlStr += '</ul></li>';
                 }
             }
             htmlStr += '</ul>\
-            </div>\
-            <div class="parent">\
-                <div class="nav-icon cross">\
-                    <div class="span"></div>\
-                </div>\
-            </div>'
+            </div>';
         $("#header").html(htmlStr);
         }
     });
