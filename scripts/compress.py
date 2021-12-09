477bf69b3c57c1aec1e6c67cabe48c2369894269
Nishant Nayak <nishantnayak2001@gmail.com>
2021-12-09 13:09:02 +0530

    Add Python scripts back
    Signed-off-by: Nishant Nayak <nishantnayak2001@gmail.com>

---
+++ scripts/compress.py
@@ -0,0 +1,8 @@
+import os
+from PIL import Image
+
+for root, dirs, files in os.walk("."):
+	for filename in files:
+		if ".png" in filename:
+			foo = Image.open(filename)
+			foo.save(filename,quality=95,optimize = True)
